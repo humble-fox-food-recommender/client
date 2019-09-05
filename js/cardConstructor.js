eb8f1fb4a7329476bd85a8d3c9f400e4918ef473
Tigor <tigor.hutasuhut@gmail.com>
2019-09-05 20:54:03 +0700

    change dashboard (again)

---
+++ js/cardConstructor.js
@@ -0,0 +1,16 @@
+function cardContructor(data) {
+    `<div class="card col-sm-3" style="width: 22rem;">
+        <img src="https://i.imgur.com/4d97aAb.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                        content.</p>
+            </div>
+            <div class="card-body">
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open
+                        Modal</button>
+            </div>
+            </div>`
+}
+
